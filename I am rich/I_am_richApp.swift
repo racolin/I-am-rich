aca7ad9b35e7dac67bfe89b24bd16e9b6dd230dc
tin <tin.pt@ntnc.vn>
2023-06-24 18:00:40 +0700

    Initial Commit

---
+++ I am rich/I_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_richApp.swift
+//  I am rich
+//
+//  Created by Administrator on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
